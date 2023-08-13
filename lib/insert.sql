f374685faa679a618f97420164264e25a08726f3
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-08-13 12:03:36 -0400

    complete project

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Choclate', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'M', 'Blue', 'High', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'M', 'Blue', 'High', True);
